5ce93652c24c08099104a76ecb364703d76f5d0e
GUchechi <godswilluchendu7@gmail.com>
2022-09-08 05:35:11 +0100

    used switch statement to trigger sounds

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ for (let i = 0; i < numberOfButtons; i++) {
        let buttonInnerHTML = this.innerHTML;
        
        switch (buttonInnerHTML) {
-        case (w):
+        case 'w':
               let tom1 = new Audio("sounds/tom-1.mp3");
               tom1.play();
             break;
-        case (a):
+        case 'a':
             let tom2 = new Audio("sounds/tom-2.mp3");
              tom2.play();
              break;
-        case (s):
+        case 's':
             let tom3 = new Audio("sounds/tom-3.mp3");
              tom3.play();
              break;
-        case (d):
+        case 'd':
             let tom4 = new Audio("sounds/tom-4.mp3");
              tom4.play();
              break;
-        case (j):
+        case 'j':
             let snare = new Audio("sounds/snare.mp3");
              snare.play();
              break;
-        case (k):
+        case 'k':
             let crash = new Audio("sounds/crash.mp3");
              crash.play();
              break;
-        case (l):
+        case 'l':
             let kickBass = new Audio("sounds/kick-bass.mp3");
              kickBass.play();
              break;       
